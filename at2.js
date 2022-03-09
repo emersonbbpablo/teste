8c828be72007a3f39a9411d37e5773bda02644c0
emersonbbpablo <emerson.bbpablo@gmail.com>
2022-03-09 19:56:25 -0300

    atualização

---
+++ at2.js
@@ -0,0 +1,12 @@
+const quantidadedealunos = 14;
+
+for (let index = 0; index <= quantidadedealunos; index ++)
+ {
+    if (index ==0){
+        console.log(' numero é zero ');
+    } else if (index % 2 ==0){
+        console.log ('o numero é par:'+" "+ index);
+        } else{
+            console.log ('o numero é impar;' +" "+index);
+        }
+        }
